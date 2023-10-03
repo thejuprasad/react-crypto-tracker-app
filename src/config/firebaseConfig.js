98b6f576a25183fa56fddc288118fc41389c2ad9
Theju Prasad <tthejuprasad00@gmail.com>
2023-10-03 16:00:49 +0530

    done

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAHYk4rPK6JlC0ygaRzfe37MJhoMdPXFqQ",
+  authDomain: "crypto-hunter-73681.firebaseapp.com",
+  projectId: "crypto-hunter-73681",
+  storageBucket: "crypto-hunter-73681.appspot.com",
+  messagingSenderId: "230662685662",
+  appId: "1:230662685662:web:823a5eadc8abc4fd83534f",
+};
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAHYk4rPK6JlC0ygaRzfe37MJhoMdPXFqQ",
+//   authDomain: "crypto-hunter-73681.firebaseapp.com",
+//   projectId: "crypto-hunter-73681",
+//   storageBucket: "crypto-hunter-73681.appspot.com",
+//   messagingSenderId: "230662685662",
+//   appId: "1:230662685662:web:823a5eadc8abc4fd83534f"
+// };
+
+export default firebaseConfig;
+
